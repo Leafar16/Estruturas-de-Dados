3a9e9afe2abe31acb9e86b3e33827cf914c39190
Rafael Perara <rafael.airosa.pereiraa@gmail.com>
2024-10-29 10:03:29 +0000

    Inserir,eliminar e imprimir lista ligada

--- Listas ligadas/SinglyLinkedList.java
+++ Listas ligadas/SinglyLinkedList.java
@@ -1,7 +1,7 @@
 import java.util.List;
 //esta implementacao nao tem em vista o principio do encapsulamneto
 public class SinglyLinkedList {
-    private ListNode head;
+    private ListNode head; //inicio da lista ligada
 
     private static class ListNode {
         private int data;
@@ -11,6 +11,70 @@ public class SinglyLinkedList {
             this.data=data;
             this.next=null;
         }
+    }
+
+    public void printList(){
+        ListNode current=head;
+        while(current!=null){
+            System.out.println(current.data);
+            current=current.next;
+        }
+    }
+
+    public int lenthList(){
+        ListNode current=head;
+        int length=0;
+        while (current!=null) {
+            length++;
+            current=current.next;
+        }
+        return length;
+    }
+
+    public void insertNode(int data){ //insere nodo no fim da lista ligada
+        ListNode current=head;
+        ListNode novo=new ListNode(data);
+        while(current.next!=null){
+            current=current.next;
+        }
+        current.next=novo;
+    }
+
+    public void insertNodeBeginning(int data){
+        ListNode novo=new ListNode(data);
+        novo.next=head;
+        head=novo;
+    }
+    
+    public void insertAtPosition(int data,int pos){
+        ListNode novo=new ListNode(data);
+        ListNode previous=head;
+        if(pos==0){
+            novo.next=head;
+            head=novo;
+        }else{
+        while(pos!=1){ 
+            pos--;
+            previous=previous.next;
+         }
+         ListNode next=previous.next;
+         previous.next=novo;
+         novo.next=next;
+        }
+    }
+
+    public void deleteFirst(){
+        ListNode current=head;
+        head=head.next;
+        current.next=null;
+    }
+
+    public void deleteLast(){
+        ListNode current=head;
+        while(current.next.next!=null){
+            current=current.next;
+        }
+        current.next=null;
     }
 
     public static void main(String[] args) {
@@ -24,7 +88,11 @@ public class SinglyLinkedList {
         sll.head.next=second; //10 --> 8
         second.next=third;  //10 --> 8 --> 34
         third.next=fourth; //10 --> 8 --> 34 --> 2 --> null
-
-
+        
+        sll.insertNode(450);
+        sll.insertNodeBeginning(200);
+        sll.insertAtPosition(90,3);
+        sll.deleteLast();
+        sll.printList();
     }
 }
