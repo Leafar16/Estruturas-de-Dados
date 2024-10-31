599fdb5f670191426edaa052ac513967448259cc
Rafael Perara <rafael.airosa.pereiraa@gmail.com>
2024-10-31 16:15:39 +0000

    Double Linked List adicionado

--- Listas ligadas/SinglyLinkedList.java
+++ Listas ligadas/SinglyLinkedList.java
@@ -119,21 +119,21 @@ public class SinglyLinkedList {
         return true;
     }
 
-    public void reverseList() {
-        if (head == null || head.next == null) return; // If the list is empty or has only one element
-    
-        ListNode previous = null;
-        ListNode current = head;
-        ListNode next = null;
-    
-        while (current != null) {
-            next = current.next; // Store the next node
-            current.next = previous; // Reverse the link
-            previous = current; // Move previous to current
-            current = next; // Move current to next
+    public void reverseList(){
+        if(head==null || head.next==null) return; //se a lista nao tem elementos ou so tem um elemento,nao se efetua alteracoes
+
+        ListNode previous=null;
+        ListNode current=head;
+        ListNode next=null;
+
+        while(current!=null){
+            next=current.next;
+            current.next=previous;
+            previous=current;
+            current=next;
         }
-    
-        head = previous; // Update head to the new first node
+
+        head=previous;
     }
 
     public ListNode findMiddleNode(){
@@ -150,9 +150,97 @@ public class SinglyLinkedList {
             current=current.next;
         }
         return current;
-
     }
     
+    public boolean detectLoop(){
+        if (head==null) return false;
+        ListNode fastPtr=head;
+        ListNode slowPtr=head;
+
+        while(fastPtr!=null && fastPtr.next!=null){//caso contrario nao poderiamos ir para next.next
+            fastPtr=fastPtr.next.next;
+            slowPtr =slowPtr.next;//se existir um loop o apontador mais lento vai eventualmente alcancar o mais rapido e o mais rapido nunca tera valor null
+            if(fastPtr==slowPtr) return true;
+        }
+        return false;
+    }
+
+    public void createALoopInLinkedList(){//usado para testar funcoes sobre loops
+        ListNode one=new ListNode(1);
+        ListNode two=new ListNode(2);
+        ListNode three=new ListNode(3);
+        ListNode four=new ListNode(4);
+        ListNode five=new ListNode(5);
+        ListNode six=new ListNode(6);
+
+        head=one;
+        one.next=two;
+        two.next=three;
+        three.next=four;
+        four.next=five;
+        five.next=six;
+        six.next=three;
+    }
+
+    public ListNode startNodeInALoop(){//using floyd's cycle detection algorithm
+        if (head==null) return null;
+        ListNode fastPtr=head;
+        ListNode slowPtr=head;
+
+        while(fastPtr!=null && fastPtr.next!=null){//caso contrario nao poderiamos ir para next.next
+            fastPtr=fastPtr.next.next;
+            slowPtr =slowPtr.next;//se existir um loop o apontador mais lento vai eventualmente alcancar o mais rapido e o mais rapido nunca tera valor null
+            if(fastPtr==slowPtr) return getStartingNode(slowPtr);
+        }
+        return null;
+    }
+
+    public ListNode getStartingNode(ListNode slowptr){
+        ListNode temp=head;
+
+        while(temp!=slowptr){
+            temp=temp.next;
+            slowptr=slowptr.next;
+        }
+        return temp; //starting node of loop
+    }
+
+
+
+    public void remove_loop(){
+        if (head==null) return ;
+        ListNode fastPtr=head;
+        ListNode slowPtr=head;
+
+        while(fastPtr!=null && fastPtr.next!=null){
+            fastPtr=fastPtr.next.next;
+            slowPtr =slowPtr.next;
+            if(fastPtr==slowPtr) removeLoopStart(slowPtr);
+        }
+        return;
+    }
+
+    public void removeLoopStart(ListNode node){
+        ListNode temp=head;
+
+        while(node.next!=temp.next){
+            temp=temp.next;
+            node=node.next;//we want to reach node before starting node
+        }
+        node.next=null;
+    }
+
+    public ListNode findNthNodeFromEnd(int pos){
+        if (head==null || pos>lengthList()) return null;
+        int nova_pos=lengthList()-pos;
+        ListNode temp=head;
+        while (nova_pos!=0){
+            temp=temp.next;
+            nova_pos--;
+        }
+        return temp;
+    }
+
     public static void main(String[] args) {
         SinglyLinkedList sll=new SinglyLinkedList();
         sll.insertNodeBeginning(10);
@@ -162,7 +250,11 @@ public class SinglyLinkedList {
         sll.insertNode(50);
         sll.insertNode(60);
 
+        SinglyLinkedList sll2=new SinglyLinkedList();
+        sll2.createALoopInLinkedList();
+        
 
+        System.out.println(sll.findNthNodeFromEnd(7).data);
         sll.printList();
     }
 }
